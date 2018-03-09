0e0f9a8faf3af6d372cd3a1e5cf15c8249972a4d
frostymaster <frostmasterwork@gmail.com>
2018-03-09 11:50:48 +0300

    Sorting

---
+++ src/com/frostmaster/algorithms/sortring/BigSorting/Solution.java
@@ -0,0 +1,42 @@
+package com.frostmaster.algorithms.sortring.BigSorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Solution {
+
+    static String[] bigSorting(String[] arr) {
+        Arrays.sort(arr, (a, b) -> StringAsIntegerCompare(a,b));
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String[] arr = new String[n];
+        for(int arr_i = 0; arr_i < n; arr_i++){
+            arr[arr_i] = in.next();
+        }
+        String[] result = bigSorting(arr);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+        }
+        System.out.println("");
+
+
+        in.close();
+    }
+
+    static int StringAsIntegerCompare(String s1, String s2)
+    {
+        if(s1.length() > s2.length()) return 1;
+        if(s1.length() < s2.length()) return -1;
+        for(int i = 0; i < s1.length(); i++)
+        {
+            if((int)s1.charAt(i) > (int)s2.charAt(i)) return 1;
+            if((int)s1.charAt(i) < (int)s2.charAt(i)) return -1;
+        }
+        return 0;
+    }
+
+}
