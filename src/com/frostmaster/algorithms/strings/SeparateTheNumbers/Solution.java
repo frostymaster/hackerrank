f5ad71fe3e5c7f2e4ed2d1f43e0b1662e874e292
ohhaithere <frostmasterwork@gmail.com>
2018-04-12 00:45:33 +0300

    moar moar moar

--- src/com/frostmaster/algorithms/strings/SeparateTheNumbers/Solution.java
+++ src/com/frostmaster/algorithms/strings/SeparateTheNumbers/Solution.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class Solution {
 
     static void separateNumbers(String s) {
+        boolean valid = false;
+        long firstx = -1;
+        for (int i=1; i<=s.length()/2; ++i) {
+            long x = Long.parseLong(s.substring(0,i));
+            firstx = x;
+            String test = Long.toString(x);
+            while (test.length() < s.length()) {
+                test += Long.toString(++x);
+            }
 
+            if (test.equals(s)) {
+                valid = true;
+                break;
+            }
+        }
+
+        System.out.println(valid ? "YES " + firstx : "NO");
     }
 
     public static void main(String[] args) {
